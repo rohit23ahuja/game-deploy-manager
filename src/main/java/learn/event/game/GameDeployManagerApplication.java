e77625011d7ace73d29e2c024a12fc4279f63538
Rohit Ahuja <rohit23ahuja@gmail.com>
2026-05-31 10:24:15 +0530

    initial commit

---
+++ src/main/java/learn/event/game/GameDeployManagerApplication.java
@@ -0,0 +1,13 @@
+package learn.event.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDeployManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDeployManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/event/game/GameDeployManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.event.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDeployManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
